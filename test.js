5f355610a54b4b3e3521699b2ff130bc95b4d156
Diego D <56505625+diego-devs@users.noreply.github.com>
2022-03-04 11:48:59 -0600

    Add follow me buttons

--- test.js
+++ test.js
@@ -1,28 +1,3 @@
-let mode = "javascript"
-const editor = CodeMirror.fromTextArea(code, 
-  {
-  lineNumbers: true,
-  styleActiveLine: true,
-  matchBrackets: true,
-  scrollbarStyle: "overlay",
-  Tab: "indentMore",
-  defaultTab: function(cm) {
-    if (cm.somethingSelected()) cm.indentSelection("add");
-    else cm.replaceSelection("  ", "end");
-  },
-  mode
-})
-editor.setOption("theme", "highcontrast-dark")
-const x = document.querySelector(".code")
-const ro = new ResizeObserver(entries => {
-  editor.setSize(10, 10)
-})
-ro.observe(document.querySelector(".code-container"))
-
-const changeMode = () => {
-  mode = mode === "css" ? "javascript" : "css"
-  editor.setOption("mode", mode)
-}
 
 
 const Diego = {
